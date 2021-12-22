4e76fd5d8e8b0afe5c18d56a6e3fdf6bfc18c6c1
faritowich <faritowich@gmail.com>
2021-12-22 08:12:23 +0300

    Cupcake app added

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cupcake
-
-import androidx.appcompat.app.AppCompatActivity
-
-/**
- * Activity for cupcake order flow.
- */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
