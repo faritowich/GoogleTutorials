8a5fd3054a2cb1a11e685c8221d360e5c9a70612
faritowich <faritowich@gmail.com>
2022-01-14 08:42:44 +0300

    Amphibians app - using MVVM, RecyclerView, Retrofit, Moshi etc.

--- Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -1,14 +1,40 @@
 package com.example.amphibians.network
 
-// TODO: Create a property for the base URL provided in the codelab
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
 
-// TODO: Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
+// Create a property for the base URL provided in the codelab
+val BASE_URL =
+    "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
 
-// TODO: Build a Retrofit object with the Moshi converter
+/*
+ * Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
+ * Moshi is an Android JSON parser that converts a JSON string into Kotlin objects.
+ */
+val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
+// Build a Retrofit object with the Moshi converter
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+//Declare a suspended function to get the list of amphibians
 interface AmphibianApiService {
-    // TODO: Declare a suspended function to get the list of amphibians
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians(): List<Amphibian>
 }
 
-// TODO: Create an object that provides a lazy-initialized retrofit service
+// Create an object that provides a lazy-initialized retrofit service
+object AmphibiansApi {
+    val retrofitService: AmphibianApiService by lazy {
+        retrofit.create(AmphibianApiService::class.java)
+    }
+}
 
